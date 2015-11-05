bc55541842114a19f7b2b6b3ad8a3cab72f323ee
nolan-m <namiller2@gmail.com>
2015-11-04 20:22:01 -0800

    Hook up Results component with Redux

--- voting-client/src/components/Results.jsx
+++ voting-client/src/components/Results.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
+import {connect} from 'react-redux';
 import Winner from './Winner';
 
-export default React.createClass({
+export const Results = React.createClass({
 	mixins: [PureRenderMixin],
 
 	getPairs: function() {
@@ -39,4 +40,14 @@ export default React.createClass({
 							</div>
 						</div>;
 	}
-})
+});
+
+function mapStateToProps(state) {
+	return {
+		pair: state.getIn(['vote', 'pair']),
+		tally: state.getIn(['vote', 'tally']),
+		winner: state.get('winner')
+	}
+}
+
+export const ResultsContainer = connect(mapStateToProps)(Results);
